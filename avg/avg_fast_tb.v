b43db5d5b9f4add188a516e02fc792b2b90caf62
preston-Glenn <44218023+preston-Glenn@users.noreply.github.com>
2021-09-22 10:37:13 -0500

    Update avg_fast_tb.v

--- avg/avg_fast_tb.v
+++ avg/avg_fast_tb.v
@@ -1,15 +1,18 @@
+
+
+
 module avg_fast_tb();
 
 	// Inputs
 
 	reg clk;
 	reg [7:0] num_in;
-    reg rs;
+   reg rs;
 	// Outputs
 	wire [7:0] out;
 
 	// Instantiate the Unit Under Test (UUT)
-	avg uut (
+	avg_fast uut (
     .num_in(num_in), 
     .clk(clk),
     .rs(rs),
@@ -19,28 +22,40 @@ module avg_fast_tb();
 		// Initialize Inputs
 		clk = 0;
 		
-
-
-		// Wait 100 ns for global reset to finish
-        clk <= clk + 1;
-        num_in <= 8'd100;
+		// Trigger Reset
+		rs <= 1;
+      clk <= clk + 1;
 		#100;
-        
-        clk <= clk + 1;
-		#100;
+		
+		clk <= clk+1;
+		#100
 
 
-        num_in <= 8'd200;
-        clk <= clk + 1;
+		rs <= 0;
+      num_in <= 8'd200;
+		
+
+      clk <= clk + 1;
 		#100;
-	// Where is the delay
+		
+
 		clk <= clk+1;
 		#100
 		
 		
+
+		clk <= clk + 1;
+      num_in <= 8'd100;
+		#100;
+        
+
+      clk <= clk + 1;
+		#100;
+		
+		
 		
 		clk <= clk + 1;
-        num_in <= 8'd100;
+      num_in <= 8'd200;
 		#100;
         
         clk <= clk + 1;
@@ -49,70 +64,61 @@ module avg_fast_tb();
 		
 		
 		clk <= clk + 1;
-        num_in <= 8'd200;
+      num_in <= 8'd255;
 		#100;
         
-        clk <= clk + 1;
+      clk <= clk + 1;
 		#100;
 		
 		
 		
 		clk <= clk + 1;
-        num_in <= 8'd255;
+      num_in <= 8'd91;
 		#100;
         
-        clk <= clk + 1;
+      clk <= clk + 1;
 		#100;
 		
 		
 		
 		clk <= clk + 1;
-        num_in <= 8'd91;
+      num_in <= 8'd25;
 		#100;
         
-        clk <= clk + 1;
+      clk <= clk + 1;
+		#100;
+		
+		
+		clk <= clk + 1;
+      num_in <= 8'd100;
+		#100;
+        
+      clk <= clk + 1;
 		#100;
 		
 		
 		
 		clk <= clk + 1;
-        num_in <= 8'd25;
+      num_in <= 8'd25;
 		#100;
         
-        clk <= clk + 1;
-		#100;
-		
-		
-		clk <= clk + 1;
-        num_in <= 8'd100;
-		#100;
-        
-        clk <= clk + 1;
+      clk <= clk + 1;
 		#100;
 		
 		
 		
 		clk <= clk + 1;
-        num_in <= 8'd25;
+      num_in <= 8'd24;
 		#100;
         
-        clk <= clk + 1;
-		#100;
-		
-		
-		
-		clk <= clk + 1;
-        num_in <= 8'd24;
-		#100;
-        
-        clk <= clk + 1;
+      clk <= clk + 1;
 		#100;
 
 
 
 
-        clk <= clk + 1;
-        rs<= 1'd1;
+      clk <= clk + 1;
+      rs<= 1'd1;
 		#100;
 
 
