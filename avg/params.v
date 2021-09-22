3f0c6f444b6079edd50b208b95c710e0f32a2041
preston-Glenn <44218023+preston-Glenn@users.noreply.github.com>
2021-09-22 10:38:15 -0500

    Create params.v

---
+++ avg/params.v
@@ -0,0 +1,11 @@
+	parameter state_Reset = 15;
+	parameter state_0 = 0;
+	parameter state_1 = 1;
+	parameter state_2 = 2;
+	parameter state_3 = 3;
+	parameter state_4 = 4;
+	parameter state_5 = 5;
+	parameter state_6 = 6;
+	parameter state_7 = 7;
+	parameter state_8 = 8;
+	parameter state_9 = 9;
